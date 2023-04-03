740c5d81ec4d0d4832c39714d0012a983d397cd4
Ángel Casas Pescador <a.casas.lp@gmail.com>
2023-04-03 22:38:41 +0100

    Update

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
+// const db = require("../models");
+const mongoose = require("mongoose");
 const cors = require('cors');
+// const { connect } = require('../controllers/wheel');
+const WebSocket = require("ws");
 const WheelSchema = require('../models/wheel.model.js');
 
 const MongoClient = require("mongodb").MongoClient;
 
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
 const DB_NAME = process.env.DB_NAME;
+const socketUrl = 'wss://socket.500.casino/socket.io/?EIO=3&transport=websocket';
+const socketOptions = {
+    headers: {
+        Origin: "https://500.casino"
+    }
+};
+// const socketUrl = process.env.WS_URL;
+// const socketOptions = { origin: process.env.ORIGIN };
 
 let cachedDb = null;
 
@@ -31,6 +44,101 @@ const queryDatabase = async (db) => {
   return results;
 };
 
+// const queryDatabase = async (db) => {
+//   const results = await db.collection("wheels").find({}).toArray();
+
+//   console.log("Results:");
+//   console.log(results);
+
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Headers": "Content-Type",
+//       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//     },
+//     body: JSON.stringify(results),
+//   };
+// };
+
+const createWheel = async (wheelData) => {
+  const db = await connectToDatabase(MONGO_DB_URI);
+  const result = await db.collection("wheels").insertOne(wheelData);
+
+  console.log("Inserted wheel with ID: ", result.insertedId);
+
+  return result;
+};
+
+const connect = () => {
+  const wheelPingIntervalTime = 10000;
+  let wheelPingInterval;
+
+  const wheelSocket = new WebSocket(socketUrl, ["websocket"], socketOptions);
+
+  console.log("[WHEEL] Connecting to WebSocket...");
+
+  wheelSocket.on("open", () => {
+    console.log("[WHEEL] Connected. Waiting for messages from WebSocket...");
+
+    wheelPingInterval = setInterval(() => {
+      if (wheelSocket.readyState === WebSocket.OPEN) {
+        wheelSocket.send("2");
+      }
+    }, wheelPingIntervalTime);
+  });
+
+  wheelSocket.on("close", () => {
+    console.log("[WHEEL] Disconnected.");
+
+    clearInterval(wheelPingInterval);
+
+    setTimeout(connect, 1000);
+  });
+
+  wheelSocket.on("message", async (data) => {
+    data = data.toString();
+
+    if (data.includes('42["round-end",')) {
+      const roundResult = JSON.parse(
+        data.replace('42["round-end",', "").slice(0, -1)
+      );
+
+      const wheelData = {
+        roundId: roundResult.winner.roundId,
+        nonce: roundResult.winner.nonce,
+        startDate: roundResult.winner.startDate,
+        endDate: roundResult.winner.endDate,
+        random: roundResult.winner.random,
+        result: WHEEL_MULTIPLIER_MAPPING[roundResult.winner.choice],
+      };
+
+      await createWheel(wheelData);
+    }
+  });
+};
+
+connect();
+
+// /**
+//  * Checks if the nonce is not the same and then creates a new Wheel object.
+//  * @param {object} wheel - The wheel object containing the information of the wheel result.
+//  * @returns {object} The new created wheel.
+//  */
+// const createWheel = async function(wheel) {
+//   // Check if the new wheel data has the same nonce as the latest wheel object in the database
+//   const lastWheel = await getLastWheel();
+//   if (lastWheel && lastWheel.nonce === wheel.nonce) {
+//       console.log(`[WHEEL] Skipping duplicate nonce ${wheel.nonce}`);
+//       return null;
+//   }
+//   return WheelSchema.create(wheel).then(docWheel => {
+//       console.log(`[WHEEL] Saved result for nonce ${docWheel.nonce}: ${wheel.result}×`);
+//       return docWheel;
+//   });
+// };
+
 module.exports.handler = async (event, context) => {
   // otherwise the connection will never complete, since
   // we keep the DB connection alive
@@ -52,6 +160,28 @@ module.exports.handler = async (event, context) => {
   };
 };
 
+// module.exports.handler = async (event, context) => {
+//   // connect to the database
+//   const db = await connectToDatabase(MONGO_DB_URI);
+
+//   // keep the socket connection alive
+//   connect();
+
+//   // execute the function every 10 seconds
+//   setInterval(() => {
+//     queryDatabase(db);
+//   }, 10000);
+
+//   // return the initial response
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ message: "Connected to database and started wheel updates." }),
+//   };
+// };
+
 const app = express();
 
 
@@ -62,27 +192,53 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// const dbName = process.env.NODE_ENV === "test" ? process.env.DB_NAME_TEST : process.env.DB_NAME;
+// const dbURI = process.env.NODE_ENV === "test" ? process.env.MONGO_DB_URI_TEST : process.env.MONGO_DB_URI;
+
+// const connection = mongoose
+//   .connect(`${dbURI}/${dbName}`, {
+//     useNewUrlParser: true,
+//     keepAlive: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("SUCCESS");
+//     connect();
+//   })
+//   .catch((err) => {
+//     console.error("Something went wrong", err)
+//   });
+
+// MONGOOSE CONNECTION
+// db.mongoose
+//     .connect(db.url, db.mongoOptions)
+//     .then(() => {
+//         // Success
+//         console.log("Successfully connected to Mongo Database.");
+//         connect();
+//     })
+//     .catch((err) => {
+//         console.error("Something went wrong.", err);
+//         process.exit();
+//     });
+
 // MIDDLEWARE
 // Parse requests of content-type - application/json
 app.use(bodyParser.json({ limit: "50mb" }));
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-
-// ROUTES
 app.get('/wheels/latest', async (req, res) => {
-  try {
-    const lastWheel = await WheelSchema.findOne().sort({ nonce: -1 }).exec();
-    console.log("SENDING");
-    res.send(lastWheel);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
-  }
-});
+    try {
+      const lastWheel = await WheelSchema.findOne().sort({ nonce: -1 }).exec();
+      console.log("SENDING");
+      res.send(lastWheel);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+  });
 
-app.get('/wheels', async (req, res) => {
-  console.log("fetch all");
-  const results = await WheelSchema.find().sort({ nonce: -1 }).limit(100);
-  res.json(results);
-});
+
+// Export app as a Netlify function
+// exports.handler = serverless(app);
