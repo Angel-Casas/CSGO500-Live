b77942dcb3c584ebb9217d17960821c1c16152eb
Ángel Casas Pescador <a.casas.lp@gmail.com>
2023-04-03 16:19:53 +0100

    Update

--- functions/api.js
+++ functions/api.js
@@ -7,49 +7,90 @@ const cors = require('cors');
 const { connect } = require('../controllers/wheel');
 const WheelSchema = require('../models/wheel.model.js');
 
-const { MongoClient } = require('mongodb');
+const MongoClient = require("mongodb").MongoClient;
 
-async function getData() {
-  const uri =
-    `${process.env.MONGO_DB_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+const DB_NAME = process.env.DB_NAME;
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
   });
 
-  try {
-    await client.connect();
-    const test = await client
-      .db('CSGO500')
-      .collection('wheels')
-      .find();
-    console.log("TEST: ");
-    console.log(test);
-    return test;
-  } catch (err) {
-    console.log(err); // output to netlify function log
-  } finally {
-    await client.close();
-  }
-}
+  cachedDb = client.db(DB_NAME);
 
-exports.handler = async function(event, context) {
-  try {
-    const data = await getData();
-    console.log("DATA:");
-    console.log(data);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data)
-    };
-  } catch (err) {
-    console.log(err); // output to netlify function log
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) 
-    };
-  }
+  return cachedDb;
 };
+
+const queryDatabase = async (db) => {
+  const results = await db.collection("wheels").find({}).toArray();
+
+  console.log("Results:");
+  console.log(results);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(results),
+  };
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGO_DB_URI);
+  return queryDatabase(db);
+};
+
+// async function getData() {
+//   const uri =
+//     `${process.env.MONGO_DB_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+//   const client = new MongoClient(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
+
+//   try {
+//     await client.connect();
+//     const test = await client
+//       .db('CSGO500')
+//       .collection('wheels')
+//       .find({});
+//     return test;
+//   } catch (err) {
+//     console.log(err); // output to netlify function log
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// exports.handler = async function(event, context) {
+//   try {
+//     const data = await getData();
+//     console.log("DATA:");
+//     console.log(data);
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(data)
+//     };
+//   } catch (err) {
+//     console.log(err); // output to netlify function log
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ msg: err.message }) 
+//     };
+//   }
+// };
 
 const app = express();
 
