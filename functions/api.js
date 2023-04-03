8adbd9a67a30b93d9fab0c5941ebbea27e04ed67
Ángel Casas Pescador <a.casas.lp@gmail.com>
2023-04-03 16:33:21 +0100

    Update

--- functions/api.js
+++ functions/api.js
@@ -38,6 +38,9 @@ const queryDatabase = async (db) => {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     },
     body: JSON.stringify(results),
   };
